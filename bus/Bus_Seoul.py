0b0cdf3b7677b07723c4336eee980d0f6d6d3495
jwhong <jwhong@bistel.co.kr>
2016-08-21 21:56:23 +0900

    update

--- bus/Bus_Seoul.py
+++ bus/Bus_Seoul.py
@@ -2,14 +2,5 @@ class Bus_Seoul :
     def __init__(self, bot):
         self.bot = bot
 
-    # Handle '/start' and '/help'
-    @bot.message_handler(commands=['help', 'start'])
-    def send_welcome(message):
-        bot.reply_to(message, """Hi there, I am Echo JwhongBot. 
-            I am hero to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!
-            """)
-
-    # Handle all other messages with content_type 'text' ( content_types defaults to ['text'])
-    @bot.message_handler(func=lambda message: True)
-    def echo_message(message):
-        bot.reply_to(message, message.text)
+    def call:
+        print('call API')
--- jwhongBot.py
+++ jwhongBot.py
@@ -4,21 +4,22 @@ API_TOKEN = '240292131:AAHCbFDidvc4KMCDNsR-CraS8_-jxz4RI0U'
 
 import bus.Bus_Seoul
 
-API_TOKEN = ''
-
 bot = telebot.TeleBot(API_TOKEN)
 
-# # Handle '/start' and '/help'
-# @bot.message_handler(commands=['help', 'start'])
-# def send_welcome(message):
-#     bot.reply_to(message, """Hi there, I am Echo JwhongBot. 
-#         I am hero to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!
-#         """)
+busSeoul = Bus_Seoul()
 
-# # Handle all other messages with content_type 'text' ( content_types defaults to ['text'])
-# @bot.message_handler(func=lambda message: True)
-# def echo_message(message):
-#     bot.reply_to(message, message.text)
+# Handle '/start' and '/help'
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    bot.reply_to(message, """Hi there, I am Echo JwhongBot. 
+        I am hero to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!
+        """)
+    print(busSeoul.call())
+
+# Handle all other messages with content_type 'text' ( content_types defaults to ['text'])
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    bot.reply_to(message, message.text)
 
 bot.polling() 
     
