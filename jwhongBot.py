8210827bcaec081e9ac5ca1cddb891a4153912a7
jwhong <jwhong@bistel.co.kr>
2016-08-21 22:07:10 +0900

    modify

--- jwhongBot.py
+++ jwhongBot.py
@@ -2,11 +2,7 @@ import telebot
 
 API_TOKEN = '240292131:AAHCbFDidvc4KMCDNsR-CraS8_-jxz4RI0U'
 
-from bus.Bus_Seoul import busSeoul
-
 bot = telebot.TeleBot(API_TOKEN)
-
-busSeoul = Bus_Seoul()
 
 # Handle '/start' and '/help'
 @bot.message_handler(commands=['help', 'start'])
@@ -14,7 +10,7 @@ def send_welcome(message):
     bot.reply_to(message, """Hi there, I am Echo JwhongBot. 
         I am hero to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!
         """)
-    print(busSeoul.call())
+    print('testMessage')
 
 # Handle all other messages with content_type 'text' ( content_types defaults to ['text'])
 @bot.message_handler(func=lambda message: True)
