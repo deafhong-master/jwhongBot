f3f1f49496824c274bb0c10f0a34c4f6f5ce6ff2
jwhong <jwhong@bistel.co.kr>
2016-08-21 22:21:28 +0900

    add bus sample

--- bus/Bus_Seoul.py
+++ bus/Bus_Seoul.py
@@ -1,6 +1,12 @@
-class Bus_Seoul :
-    def __init__(self, bot):
-        self.bot = bot
 
-    def call:
-        print('call API')
+
+from urllib2 import Request, urlopen
+from urllib import urlencode, quote_plus
+
+url = 'http://ws.bus.go.kr/api/rest/arrive/getArrInfoByRoute'
+queryParams = '?' + urlencode({ quote_plus('ServiceKey') : '서비스키', quote_plus('stId') : '', quote_plus('busRouteId') : '', quote_plus('numOfRows') : '999', quote_plus('pageNo') : '1' })
+
+request = Request(url + queryParams)
+request.get_method = lambda: 'GET'
+response_body = urlopen(request).read()
+print response_body
