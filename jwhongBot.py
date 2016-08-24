65ea9b708864a9a0f5234e35f003ba72f4728416
jwhong <jwhong@bistel.co.kr>
2016-08-24 18:20:42 +0900

    modify

--- jwhongBot.py
+++ jwhongBot.py
@@ -1,6 +1,9 @@
 import telebot
 
-from bus.bus_seoul import bus_seoul
+# from bus.bus_seoul import bus_seoul
+
+from urllib2 import Request, urlopen
+from urllib import urlencode, quote_plus
 
 API_TOKEN = '240292131:AAHCbFDidvc4KMCDNsR-CraS8_-jxz4RI0U'
 
@@ -16,6 +19,13 @@ def send_welcome(message):
 
 @bot.message_handler(commands=['bus'])
 def bus_info(message):
+    url = 'http://ws.bus.go.kr/api/rest/arrive/getArrInfoByRoute'
+    queryParams = '?' + urlencode({ quote_plus('ServiceKey') : 'nuD0VA4oaDhlyilVJNYSxhuI8x23SqQQN2kHmPkbklAUhhLmy0UioY92z6yQtx0oCLl6DlNrk%2FkTwkkHhwfvcA%3D%3D', quote_plus('stId') : '22274', quote_plus('busRouteId') : '3412', quote_plus('numOfRows') : '999', quote_plus('pageNo') : '1' })
+
+    request = Request(url + queryParams)
+    request.get_method = lambda: 'GET'
+    response_body = urlopen(request).read()
+    print response_body
 	bot.reply_to(message, bus_seoul.call())
 
 # Handle all other messages with content_type 'text' ( content_types defaults to ['text'])
@@ -25,3 +35,7 @@ def echo_message(message):
 
 bot.polling() 
     
+
+
+
+
